8ed3fff6d7e08d00f17ee5faef1f8163a80fde47
James <jlbaker361@gmail.com>
2025-04-29 12:50:07 -0400

    none

---
+++ run_workers.sh
@@ -0,0 +1,12 @@
+M=$1  # Get the first command-line argument
+C=$2
+# Check if it's a valid integer
+if ! [[ "$count" =~ ^[0-9]+$ ]]; then
+    echo "Please provide a positive integer."
+    exit 1
+fi
+
+# Loop from 1 to count
+for ((i = 1; i <= count; i++)); do
+    go run worker.go -C="$C" -config=config.txt &
+done
